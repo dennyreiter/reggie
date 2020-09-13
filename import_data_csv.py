3bf18a94265042a717ad71504e5c96fd30ef5a91
Denny Reiter <denny@reiters.org>
2020-09-13 11:54:01 -0500

    script to import the quotes, along with all the current quotes

---
+++ import_data_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+    Script to import book data from .csv file to Model Database DJango
+    To execute this script run: 
+                                1) manage.py shell
+                                2) exec(open('import_data_csv.py').read())
+"""
+
+__author__ = "Rafael García Cuéllar"
+__email__ = "r.gc@hotmail.es"
+__copyright__ = "Copyright (c) 2018 Rafael García Cuéllar"
+__license__ = "MIT"
+
+import csv
+from quotes.models import Quotation, Category
+
+CSV_PATH = 'quote_data/dad-joke.txt'
+CAT_ID = 12
+
+contSuccess = 0
+# Remove all data from Table
+#Book.objects.all().delete()
+
+with open(CSV_PATH, newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter='|', quotechar=';')
+    print('Loading...')
+    for row in spamreader:
+        Quotation.objects.create(category=Category.objects.get(pk = (CAT_ID)), quote=row[0])
+        contSuccess += 1
+    print(f'{str(contSuccess)} inserted successfully! ')
+
