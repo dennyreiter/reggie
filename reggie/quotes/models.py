b60a84f290be9cd7ad8bae9fe1633b7b71a04fc0
Denny Reiter <denny@reiters.org>
2020-09-13 11:57:39 -0500

    add my db models

--- reggie/quotes/models.py
+++ reggie/quotes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Quotation(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quote = models.CharField(max_length=400)
+    count = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    last_served = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Quotations"
+
+    def __str__(self):
+        return self.quote
+
