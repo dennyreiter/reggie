9809cdcb651c67e08765549ec910a9ffb3d26df8
Denny Reiter <denny@reiters.org>
2020-09-13 21:56:18 -0500

    move the object save until after the data isready to render so that the last served is really the last served and not right now.  Also check to make sure that there is a queryset returned, if not likely because the category doesnnot exist, so redirect to the random quote.

--- reggie/quotes/views.py
+++ reggie/quotes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 #from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.generic import TemplateView
@@ -16,7 +16,6 @@ def quote(request):
     items = Quotation.objects.all()
     random_item = random.choice(items)
     random_item.count += 1
-    random_item.save()
     data = {
             'category': model_to_dict(random_item.category),
             'quote': random_item.quote,
@@ -24,18 +23,22 @@ def quote(request):
             'last_served': random_item.last_served,
             'count': random_item.count 
             }
+    random_item.save()
     return JsonResponse(data)
 
 def categorical_quote(request, request_category):
     items = Quotation.objects.filter(category__name=request_category)
-    random_item = random.choice(items)
-    random_item.count += 1
-    random_item.save()
-    data = {
-            'category': model_to_dict(random_item.category),
-            'quote': random_item.quote,
-            'created': random_item.created,
-            'last_served': random_item.last_served,
-            'count': random_item.count 
-            }
-    return JsonResponse(data)
+    if items.exists():
+        random_item = random.choice(items)
+        random_item.count += 1
+        data = {
+                'category': model_to_dict(random_item.category),
+                'quote': random_item.quote,
+                'created': random_item.created,
+                'last_served': random_item.last_served,
+                'count': random_item.count 
+                }
+        random_item.save()
+        return JsonResponse(data)
+    else:
+        return redirect(quote)
