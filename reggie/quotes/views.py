70f27b66f6b2bc85d6ff6587dbcd618732ca09fa
Denny Reiter <denny@reiters.org>
2021-01-10 19:28:44 -0600

    make the request be all lower case

--- reggie/quotes/views.py
+++ reggie/quotes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+import django.views.debug
 from django.forms.models import model_to_dict
 from .models import Quotation, Category
 import random
@@ -11,6 +13,10 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+def check_status(request):
+    data = get_traceback_data()
+    return JsonResponse(data)
 
 def quote(request):
     items = Quotation.objects.all()
@@ -27,7 +33,7 @@ def quote(request):
     return JsonResponse(data)
 
 def categorical_quote(request, request_category):
-    items = Quotation.objects.filter(category__name=request_category)
+    items = Quotation.objects.filter(category__name=request_category.lower())
     if items.exists():
         random_item = random.choice(items)
         random_item.count += 1
@@ -42,3 +48,22 @@ def categorical_quote(request, request_category):
         return JsonResponse(data)
     else:
         return redirect(quote)
+
+def search_quote(request, query):
+    items = Quotation.objects.filter(quote__icontains=query)
+    datum = []
+    for item in items:
+        data = {
+                'category': model_to_dict(item.category),
+                'quote': item.quote,
+                'created': item.created,
+                'last_served': item.last_served,
+                'count': item.count 
+                }
+        datum += data
+    return HttpResponse(datum)
+
+class SearchView(ListView):
+    template_name = 'search.html'
+    paginate_by = 10
+    count = 0
