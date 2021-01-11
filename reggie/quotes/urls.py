a475ed68592f4c37ead1d878d0acdea1a51fa4bd
Denny Reiter <denny@reiters.org>
2021-01-10 19:25:25 -0600

    fix urls

--- reggie/quotes/urls.py
+++ reggie/quotes/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, quote, categorical_quote
+from .views import HomePageView, AboutPageView, quote, categorical_quote, check_status, search_quote
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('quote/<request_category>', categorical_quote, name='categorical_quote'),
+    path('quote/<request_category>/', categorical_quote, name='categorical_quote'),
     path('quote/', quote, name='quote'),
+    path('search/<query>', search_quote, name='search'),
     path('', HomePageView.as_view(), name='home'),
+    path('check/', check_status, name='check'),
 ]
